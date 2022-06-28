4671d7fbfaa09fe02541bc767f1425ab2070aa83
Gabriel Santo <gvgesanto2@gmail.com>
2022-06-28 20:46:42 -0300

    fix: Fix linters errors

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-import "./styles/global.scss";
+import './styles/global.scss';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
