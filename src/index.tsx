711419e24d29ae7a4ca8a00348e68087a10f6282
Gabriel Santo <gvgesanto2@gmail.com>
2022-06-28 21:05:17 -0300

    Test eslint in Github Actions

--- src/index.tsx
+++ src/index.tsx
@@ -6,9 +6,9 @@ import './styles/global.scss';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
-);
+)
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-);
+)
